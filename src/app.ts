79325b6472bd01de7a6ac37c4587af200d8d881c
opuopu <nurmdopu428@gmail.com>
2024-09-19 11:29:43 +0600

    Fix:fixed issue

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["*"], // Allow all origins
+    origin: true, // Allow all origins
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
