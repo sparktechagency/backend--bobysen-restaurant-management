a2ed5ef6f1c0be531f88a583cfa252ad2c37984d
opuopu <nurmdopu428@gmail.com>
2024-11-22 16:55:45 +0600

    fix

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -24,8 +24,7 @@ const getimnCallback = catchAsync(async (req: Request, res: Response) => {
 const loadPaymentZone = catchAsync(async (req: Request, res: Response) => {
   const body = { ...req.body };
   body.user = req?.user?.userId;
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NmMwOTExMGZmNDkxZWVlYmJhMzAwMmQiLCJyb2xlIjoidXNlciIsImlhdCI6MTczMjI3MTc4MCwiZXhwIjoxNzM0ODYzNzgwfQ.0tvyp3jqYaVNFryP0OP4Ksgsh3cZDlOLQ0mIzFaml2U";
+  const token = req.headers.authorization?.split(" ")[1];
   console.log(token);
   const result = await orderServices.loadPaymentZone(body, token as string);
   sendResponse(res, {
