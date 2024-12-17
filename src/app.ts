d2377947387d0e0a27a65f784c9e5e286a86699a
opuopu <nurmdopu428@gmail.com>
2024-12-17 15:14:16 +0600

    Fix

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ app.use(
       "https://mybookatable.mu",
       "https://bookatable.mu",
       "https://socket.bookatable.mu",
+      "http://localhost:5173",
     ], // Allow all origins
     // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
