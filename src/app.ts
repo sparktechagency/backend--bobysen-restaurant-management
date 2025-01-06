bb1eb775ad0c662b195294a5d877675c0814c78e
opuopu <nurmdopu428@gmail.com>
2025-01-06 17:56:55 +0600

    feat: feat added restaurant selection part

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(
       "https://socket.bookatable.mu",
       "http://localhost:5173",
     ], // Allow all originss
-    credentials: true,
+    // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
