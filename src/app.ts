f069f621c35e899e2d54d18903fa7ac54ac520e2
opuopu <nurmdopu428@gmail.com>
2025-03-27 15:20:17 +0600

    feat: added github workflows

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use(
       "https://socket.bookatable.mu",
       "http://localhost:5173",
       "http://localhost:3000",
+      "https://reservation.bookatable.mu/",
     ], // Allow all originss
     // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
